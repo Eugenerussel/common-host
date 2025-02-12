5ffe7bcfd9a65e2075b250d4a7bdced9d3f3067f
Eugenerussel <eugenerussel007@gmail.com>
2025-02-12 11:34:47 +0530

    added images

--- host-app/src/app/app.routes.ts
+++ host-app/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'businessOperation',pathMatch:'full'},
-    { path:'businessOperation',
+    {path:'',redirectTo:'/login',pathMatch:'full'},
+    {path:'login',component:HomeComponent},
+    {path:'businessOperation',
         loadChildren: () => loadRemoteModule({
         remoteEntry: 'http://localhost:4201/remoteEntry.js',
         type: 'module',
---
+++ host-app/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  constructor(private authService: AuthService,private router:Router) {}
+
+  ngOnInit() {}
+
+  lambdaLogin() {
+    this.authService.login();
+    this.router.navigate(['/businessOperation']);
+  }
+
+}
